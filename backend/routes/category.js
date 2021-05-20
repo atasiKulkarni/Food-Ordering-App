e6d8615cf02e9f5dc694945939829441e1e825c7
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-05-20 19:28:53 +0530

    Add files via upload

--- backend/routes/category.js
+++ backend/routes/category.js
@@ -1,1 +1,21 @@
+const router = require("express").Router();
+let Category = require("../models/category.model");
 
+router.route("/").get((req, res) => {
+  Category.find()
+    .then((category) => res.json(category))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const restaurantname = req.body.restaurantname;
+  const category = req.body.category;
+
+  const newCategory = new Category({ restaurantname, category });
+
+  newCategory
+    .save()
+    .then(() => res.json("Category added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+module.exports = router;
---
+++ backend/routes/menu.js
@@ -0,0 +1,51 @@
+const router = require("express").Router();
+let Menu = require("../models/menu.model");
+
+router.route("/").get((req, res) => {
+  Menu.find()
+    .then((menu) => res.json(menu))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/add").post((req, res) => {
+  const restaurantname = req.body.restaurantname;
+  const category = req.body.category;
+  const menuitem = req.body.menuitem;
+  const price = Number(req.body.price);
+
+  const newMenu = new Menu({ restaurantname, category, menuitem, price });
+
+  newMenu
+    .save()
+    .then(() => res.json("Menu added!"))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").get((req, res) => {
+  Menu.findById(req.params.id)
+    .then((menu) => res.json(menu))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/:id").delete((req, res) => {
+  Menu.findByIdAndDelete(req.params.id)
+    .then(() => res.json("Menu deleted."))
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+router.route("/update/:id").post((req, res) => {
+  Menu.findById(req.params.id)
+    .then((menu) => {
+      // menu.category = req.body.category;
+      menu.menuitem = req.body.menuitem;
+      menu.price = Number(req.body.price);
+
+      menu
+        .save()
+        .then(() => res.json("Menu updated!"))
+        .catch((err) => res.status(400).json("Error: " + err));
+    })
+    .catch((err) => res.status(400).json("Error: " + err));
+});
+
+module.exports = router;
