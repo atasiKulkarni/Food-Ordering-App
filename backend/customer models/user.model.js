ca8e062263cc904239745339d056a088d171d864
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-05-20 19:23:39 +0530

    Add files via upload

---
+++ backend/customer models/user.model.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const signupSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", signupSchema);
+
+module.exports = User;
