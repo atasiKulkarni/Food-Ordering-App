0c02608851c62a281dcae54b14d4c7d5532f3746
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-05-20 19:25:37 +0530

    Add files via upload

--- backend/models/category.model.js
+++ backend/models/category.model.js
@@ -1,1 +1,25 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema(
+  {
+    restaurantname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    category: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Category = mongoose.model("Category", categorySchema);
+
+module.exports = Category;
---
+++ backend/models/menu.model.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const menuSchema = new Schema(
+  {
+    restaurantname: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    category: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+    menuitem: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+    price: {
+      type: Number,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Menu = mongoose.model("Menu", menuSchema);
+
+module.exports = Menu;
