d2279df73ab264fc8b3ebeeb08ce35368ed6e8b6
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-05-20 19:15:14 +0530

    Add files via upload

--- backend/bin/business models/business-user.model.js
+++ backend/bin/business models/business-user.model.js
@@ -1,1 +1,58 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const businessSignupSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 3,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    restaurantname: {
+      type: String,
+      required: true,
+      minlength: 3,
+    },
+    phone: {
+      type: Number,
+      required: true,
+      minlength: 3,
+    },
+    address: {
+      type: String,
+      required: true,
+      minlength: 7,
+    },
+    city: {
+      type: String,
+      required: true,
+      minlength: 2,
+    },
+    cnic: {
+      type: String,
+      required: true,
+      minlength: 2,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const businessUser = mongoose.model("Business User", businessSignupSchema);
+
+module.exports = businessUser;
