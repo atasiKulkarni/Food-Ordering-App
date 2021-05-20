5592a1fc8d432e4dc0cd42ef9c5b564ca46de778
Atasi Kulkarni <75190043+atasikulkarni@users.noreply.github.com>
2021-05-20 19:17:08 +0530

    Add files via upload

--- backend/bin/customer models/order.model.js
+++ backend/bin/customer models/order.model.js
@@ -1,1 +1,39 @@
+const mongoose = require("mongoose");
 
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+  {
+    restaurantname: String,
+    name: String,
+    dinein: {
+      type: Boolean,
+      required: true,
+    },
+    tablereservation: {
+      type: Boolean,
+      required: true,
+    },
+    people: {
+      type: Number,
+    },
+    status: String,
+    order: {
+      products: [
+        {
+          item: String,
+          price: Number,
+          quantity: Number,
+        },
+      ],
+      total: Number,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+module.exports = Order;
